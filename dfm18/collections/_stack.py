2071d1b75b4c6a8e930c2c7329596e71eaae67c6
Daniel Fortich <fortichdaniel16@gmail.com>
2025-01-10 20:39:31 -0500

    format ._stack.py

--- dfm18/collections/_stack.py
+++ dfm18/collections/_stack.py
@@ -9,50 +9,50 @@ class Stack[T]:
     def __init__(self):
         self.top = None
         self.size = 0
-    
+
     def push(self, data: T):
         node = Node(data)
-        
+
         if self.top:
             node.next = self.top
             self.top = node
         else:
             self.top = node
-        
+
         self.size += 1
-    
+
     def pop(self) -> T:
         if self.top:
             data = self.top.data
             self.size -= 1
-            
+
             if self.top.next:
                 self.top = self.top.next
             else:
                 self.top = None
-            
+
             return data
         else:
             raise EmptyStackException("Stack is empty")
-    
+
     def peek(self) -> T:
         if self.top:
             return self.top.data
         else:
             raise EmptyStackException("Stack is empty")
-    
+
     def contains(self, target: T) -> bool:
         current = self.top
-        
+
         while current:
             if current.data == target:
                 return True
             current = current.next
         return False
-    
+
     def __contains__(self, target: T) -> bool:
         return self.contains(target)
-    
+
     def clear(self):
         while self.top:
             self.pop()
