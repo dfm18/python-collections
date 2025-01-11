55648437b3fcb35f04587a55073483dd8417f008
Daniel Fortich <fortichdaniel16@gmail.com>
2025-01-10 20:34:32 -0500

    format ._array.py

--- dfm18/collections/_array.py
+++ dfm18/collections/_array.py
@@ -1,4 +1,4 @@
-from typing import  Optional, Iterator
+from typing import Optional, Iterator
 
 
 class Array[T]:
@@ -8,23 +8,22 @@ class Array[T]:
         self._items = list()
         for _ in range(capacity):
             self._items.append(fill_value)
-    
+
     def __len__(self) -> int:
         return len(self._items)
-    
+
     def __str__(self) -> str:
         return f"Array({self._items})"
-    
+
     def __iter__(self) -> Iterator[T]:
         return iter(self._items)
-    
+
     def __getitem__(self, index: int) -> T:
         return self._items[index]
-    
+
     def __setitem__(self, index: int, new_item: T):
         self._items[index] = new_item
-    
+
     @property
     def capacity(self) -> int:
         return len(self._items)
-    
